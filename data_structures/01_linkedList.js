f1cb637d5eafb140392bd2467aa5e7bfaa4d8f31
DeoSx <Deosxz123@gmail.com>
2021-01-02 16:05:29 +0600

    add linked list

---
+++ data_structures/01_linkedList.js
@@ -0,0 +1,184 @@
+class LinkedListNode {
+  constructor(value, next = null) {
+    this.value = value
+    this.next = next
+  }
+  toString(callback) {
+    callback ? callback(this.value) : `${this.value}`
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null
+    this.tail = null
+  }
+
+  prepend(value) {
+    const newNode = new LinkedListNode(value, this.head)
+    this.head = newNode
+
+    if (!this.tail) {
+      this.tail = newNode
+    }
+    return this
+  }
+
+  append(value) {
+    const newNode = new LinkedListNode(value)
+
+    if (!this.head || !this.tail) {
+      this.head = newNode
+      this.tail = newNode
+    }
+
+    this.tail.next = newNode
+    this.tail = newNode
+
+    return this
+  }
+
+  delete(value) {
+    if (!this.head) {
+      return null
+    }
+    let deletedNode = null
+
+    while (this.head && this.head.value === value) {
+      deletedNode = this.head
+      this.head = this.head.next
+    }
+
+    let currentNode = this.head
+
+    if (currentNode !== null) {
+      while (currentNode.next) {
+        if (currentNode.next.value === value) {
+          deletedNode = currentNode.next
+          currentNode.next = currentNode.next.next
+        } else {
+          currentNode = currentNode.next
+        }
+      }
+    }
+
+    if (this.tail && this.tail.value === value) {
+      this.tail = currentNode
+    }
+    return deletedNode
+  }
+
+  deleteTail() {
+    if (!this.head) {
+      return null
+    }
+
+    const deletedTail = this.tail
+
+    if (this.head === this.tail) {
+      this.head = null
+      this.tail = null
+
+      return deletedTail
+    }
+
+    let currentNode = this.head
+
+    while (currentNode.next) {
+      if (!currentNode.next.next) {
+        currentNode.next = null
+      } else {
+        currentNode = currentNode.next
+      }
+    }
+
+    this.tail = currentNode
+
+    return deletedTail
+  }
+
+  deleteHead() {
+    if (!this.head) {
+      return null
+    }
+
+    const deletedHead = this.head
+
+    if (this.head.next) {
+      this.head = this.head.next
+    } else {
+      this.head = null
+      this.tail = null
+    }
+
+    return deletedHead
+  }
+
+  find(value) {
+    if (!this.head) {
+      return null
+    }
+
+    let currentNode = this.head
+
+    while (currentNode) {
+      if (value !== undefined && currentNode.value === value) {
+        return currentNode
+      }
+      currentNode = currentNode.next
+    }
+
+    return null
+  }
+
+  fromArray(values) {
+    values.forEach((value) => this.append(value))
+  }
+
+  toArray() {
+    const nodes = []
+
+    let currentNode = this.head
+
+    while (currentNode) {
+      nodes.push(currentNode)
+      currentNode = currentNode.next
+    }
+    return nodes
+  }
+
+  toString(callback) {
+    return this.toArray()
+      .map((node) => node.toString(callback))
+      .toString()
+  }
+
+  reverse() {
+    let currNode = this.head
+    let prevNode = null
+    let nextNode = null
+
+    while (currNode) {
+      nextNode = currNode.next
+      currNode.next = prevNode
+      prevNode = currNode
+      currNode = nextNode
+    }
+
+    this.tail = this.head
+    this.head = prevNode
+
+    return this
+  }
+}
+
+const linkedListData = new LinkedList()
+
+linkedListData.append('one')
+linkedListData.append('two')
+linkedListData.append('three')
+linkedListData.append('four')
+linkedListData.append('five')
+linkedListData.prepend('zero')
+
+console.log(linkedListData.toArray())
